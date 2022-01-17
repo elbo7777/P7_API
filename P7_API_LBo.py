2361b5dbc3d97afb96aaff960354b120249627d3
LAURENT BOIN <86362509+elbo7777@users.noreply.github.com>
2022-01-17 16:16:08 +0100

    Add files via upload

--- P7_API_LBo.py
+++ P7_API_LBo.py
@@ -5,10 +5,14 @@ import pandas as pd
 # Load our model with pickle (adresses may be changed for serialisation)
 model = pickle.load(open('banking_model.md', 'rb'))
 # Load data test sample (index_col = 0 to keep id of loan)
-df_test_sample = pd.read_csv('app_test_1000.csv', sep=',', index_col=0, encoding='utf8')
+# df_test_sample = pd.read_csv('app_test_1000.csv', sep=',', index_col=0, encoding='utf8')
+df_test_sample = pd.read_csv('app_test_1000.csv', index_col = 0, encoding = 'utf8')
+
+# Drop predict last column predict proba of our dataframe not used here
+df_test_sample.drop(columns = 'TARGET', inplace = True)
 
 # On définit le seuil de non solvabilité => si proba > seuil alors non solvable
-treshold = 0.10
+seuil = 0.10
 # defining flask pages
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -28,7 +32,8 @@ def predict():
     model_prediction = model.predict_proba(input.values.reshape(1,-1))
     # create a dictionnary to janosify. We get the probability to be positive and evaluate if the credit is accepted (1) or denied (0)!
     # careful bool object must be transformed in int for JSON format!
-    dict_result = {'ID_loan':int(data_index), 'Credit_score': model_prediction[:,1][0], 'Answer': int(model_prediction[:,1][0] < treshold)}
+    # dict_result = {'ID Client':int(data_index), 'Credit_score': model_prediction[:,1][0], 'Réponse (1 : Non Solvable 0 Solvable)': int(model_prediction[:,1][0] < treshold)}
+    dict_result = {'Credit_score': model_prediction[:,1][0], 'Reponse (1 : Solvable 0 Non Solvable)': int(model_prediction[:,1][0] < seuil),'ID Client':int(data_index), }
     return flask.jsonify(dict_result)
 
 # define endpoint for Flask
